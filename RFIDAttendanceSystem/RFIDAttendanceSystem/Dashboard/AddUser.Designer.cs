7db5453570acb298dea5a78587eb61dac194ddc1
Stephann24 <stephannbrosas68@gmail.com>
2025-11-08 10:56:41 +0800

    Add files via upload

---
+++ RFIDAttendanceSystem/RFIDAttendanceSystem/Dashboard/AddUser.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace RFIDAttendanceSystem.Dashboard
+{
+    partial class AddUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddUser));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TBuserid = new System.Windows.Forms.TextBox();
+            this.TBfirstname = new System.Windows.Forms.TextBox();
+            this.TBmiddlename = new System.Windows.Forms.TextBox();
+            this.TBlastname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(598, 38);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(554, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(46, 139);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(162, 153);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TBuserid
+            // 
+            this.TBuserid.Location = new System.Drawing.Point(232, 105);
+            this.TBuserid.Multiline = true;
+            this.TBuserid.Name = "TBuserid";
+            this.TBuserid.Size = new System.Drawing.Size(323, 30);
+            this.TBuserid.TabIndex = 2;
+            // 
+            // TBfirstname
+            // 
+            this.TBfirstname.Location = new System.Drawing.Point(232, 171);
+            this.TBfirstname.Multiline = true;
+            this.TBfirstname.Name = "TBfirstname";
+            this.TBfirstname.Size = new System.Drawing.Size(323, 30);
+            this.TBfirstname.TabIndex = 3;
+            // 
+            // TBmiddlename
+            // 
+            this.TBmiddlename.Location = new System.Drawing.Point(232, 233);
+            this.TBmiddlename.Multiline = true;
+            this.TBmiddlename.Name = "TBmiddlename";
+            this.TBmiddlename.Size = new System.Drawing.Size(323, 30);
+            this.TBmiddlename.TabIndex = 4;
+            this.TBmiddlename.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // TBlastname
+            // 
+            this.TBlastname.Location = new System.Drawing.Point(232, 295);
+            this.TBlastname.Multiline = true;
+            this.TBlastname.Name = "TBlastname";
+            this.TBlastname.Size = new System.Drawing.Size(323, 30);
+            this.TBlastname.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(232, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "User ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(232, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "First Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(232, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Middle Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(232, 275);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Last Name:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(232, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(323, 50);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Enter";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // AddUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(43)))));
+            this.ClientSize = new System.Drawing.Size(598, 417);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TBlastname);
+            this.Controls.Add(this.TBmiddlename);
+            this.Controls.Add(this.TBfirstname);
+            this.Controls.Add(this.TBuserid);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AddUser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AddUser";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TBuserid;
+        private System.Windows.Forms.TextBox TBfirstname;
+        private System.Windows.Forms.TextBox TBmiddlename;
+        private System.Windows.Forms.TextBox TBlastname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ RFIDAttendanceSystem/RFIDAttendanceSystem/Dashboard/AddUser.cs
@@ -0,0 +1,64 @@
+﻿using RFIDAttendanceSystem.DataBase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RFIDAttendanceSystem.Dashboard
+{
+    public partial class AddUser : Form
+    {
+        public static DatabaseConnector connector = new DatabaseConnector();
+        public string ConnectionString = connector.GetConnection();
+
+        public AddUser()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void SaveUserRecord()
+        {
+            try {
+                using (SqlConnection conn = new SqlConnection(ConnectionString)) 
+                using (SqlCommand command = new SqlCommand("SaveUserRecords", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("user_id", TBuserid.Text.ToString());
+                    command.Parameters.AddWithValue("first_name", TBfirstname.Text.ToString());
+                    command.Parameters.AddWithValue("middle_name", TBmiddlename.Text.ToString());
+                    command.Parameters.AddWithValue("last_name", TBlastname.Text.ToString());
+
+                    conn.Open();
+                    command.ExecuteNonQuery();
+                    conn.Close();
+
+                    MessageBox.Show("User Record Save Successfully");
+                }
+            }
+            catch 
+            { 
+
+            }
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveUserRecord();
+        }
+    }
+}
